7e6d3c4eb6613887d75ccff8cfe6addc1a09356f
emiliano <68301161+soetgdeznsgk@users.noreply.github.com>
2024-09-25 13:22:33 -0500

    añado cuerpos CSG para implementar la transparencia en los charcos

--- logic/scenes/splot.gd
+++ logic/scenes/splot.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@onready var holes_ref := preload("res://logic/scenes/splot_hole.tscn")
+
 func _ready() -> void:
 	transform = transform.rotated_local(Vector3.UP, randf() * 4) 
 	#TODO Ésta rotación va a ser un dolor de huevos en adelantel, recordar hacer 
