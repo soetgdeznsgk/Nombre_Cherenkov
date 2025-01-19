18df419d2c0d2e4c4fcf2157160465b2df4ccd67
emiliano <68301161+soetgdeznsgk@users.noreply.github.com>
2025-01-19 10:30:48 -0500

    añado primer borrador de la sala de control y alarmas

--- logic/scenes/camarascript.gd
+++ logic/scenes/camarascript.gd
@@ -41,6 +41,8 @@ func _input(event):
 		#bucket_reference.rotate_to_camera(event.relative.x) # no se necesita todos los frames
 		v.x = clamp(v.x,-80,90)
 		
+	elif Input.is_key_label_pressed(KEY_C): # DEBUG mientras se ata a el presionar un botón
+		get_tree().call_group("Alarmables", "start")
 
 func _physics_process(delta):
 	if not locked:
