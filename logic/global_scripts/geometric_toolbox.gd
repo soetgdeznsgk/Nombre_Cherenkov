3cdccec16f7af5a6f2a62b0c438ae300e566f830
emiliano <68301161+soetgdeznsgk@users.noreply.github.com>
2025-01-30 09:45:47 -0500

    regulo la velocidad de movimiento en una rampa

--- logic/global_scripts/geometric_toolbox.gd
+++ logic/global_scripts/geometric_toolbox.gd
@@ -3,5 +3,3 @@ class_name Geometric_toolbox # convertir ésto en una clase global, pero no sé 
 
 func y_offset_vector_to_0(v : Vector3) -> Vector3:
 	return v - (Vector3.UP * v.y)
-
-#class_name MinHeapVector3
